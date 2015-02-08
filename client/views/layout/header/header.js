634437ea562135fa25b8acf1a278bc097ffbe16d
Matyas Albert Nagy <albertmatyi@gmail.com>
2015-02-08 15:13:58 +0100

    fixed routes & added headr & footer

---
+++ client/views/layout/header/header.js
@@ -0,0 +1,40 @@
+/**
+ * Created by albertmatyi on 2/8/15.
+ */
+'use strict';
+
+Template.header.helpers({
+	pages: function() {
+		return [
+			{
+				routeName: 'home',
+				title: 'Home'
+			}
+//			, {
+//				routeName: 'rooms',
+//				title: 'Rooms'
+//			}
+//			, {
+//				routeName: 'rooms',
+//				title: 'Rooms'
+//			}
+		];
+	}
+});
+
+Template.headerPageLink.helpers({
+	active: function() {
+		var current = Router.current();
+		if (!current) {
+			return;
+		}
+		var route = current.route;
+		if (!route || !route.getName) {
+			return;
+		}
+		return this.routeName === route.getName() ? 'active' : '';
+	}
+});
+
+
+
