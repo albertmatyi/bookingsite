d6e6fe176b34b0654e2830d1569b0dc62c24858f
Matyas Albert Nagy <albertmatyi@gmail.com>
2015-02-25 21:07:32 +0100

    added alerts

---
+++ client/elements/alerts/alerts.js
@@ -0,0 +1,33 @@
+Alerts.defaultOptions = {
+	/**
+	 * Button with cross icon to hide (close) alert
+	 */
+	dismissable: true,
+	/**
+	 * CSS classes to be appended on each alert DIV (use space for separator)
+	 */
+	classes: 'col-xs-12 ' +
+	'col-sm-8 col-sm-offset-2 ' +
+	'col-md-6 col-md-offset-3 ' +
+	'col-lg-4 col-lg-offset-4',
+	/**
+	 * Hide alert after delay in ms or false to infinity
+	 */
+	autoHide: 3000,
+	/**
+	 * Time in ms before alert fully appears
+	 */
+	fadeIn: 200,
+	/**
+	 * If autoHide enabled then fadeOut is time in ms before alert disappears
+	 */
+	fadeOut: 600,
+	/**
+	 * Amount of alerts to be displayed
+	 */
+	alertsLimit: 3,
+	/**
+	 * Allows use HTML in messages
+	 */
+	html: false
+};
