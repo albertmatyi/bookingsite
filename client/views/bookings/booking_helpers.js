839dabd79ae917d9ef3f769beb0e8ef2e9c3abd0
Matyas Albert Nagy <albertmatyi@gmail.com>
2015-02-25 20:32:42 +0100

    initialize booking session vars

--- client/views/bookings/booking_helpers.js
+++ client/views/bookings/booking_helpers.js
@@ -2,57 +2,10 @@
 
 Template.bookingForm.helpers({});
 
-var beforeShowDay = function(date) {
-	date = App.date.stripTime(date);
-	if (date < App.date.today) {
-		return {'enabled': false};
-	}
-	var isEndDatepicker = this.element ?
-	this.element.prop('id').indexOf('end') !== -1 : false;
-	// this.date refers to the date that is about to be selected selection
-	var startDate = isEndDatepicker ?
-		Session.get('booking.form.booking.startDate') : App.date.stripTime(this.date);
-	var endDate = isEndDatepicker ?
-		App.date.stripTime(this.date) : Session.get('booking.form.booking.endDate');
-	var inRange = date >= startDate && date <= endDate;
-	var ret = null;
-	if (isEndDatepicker) {
-		date = App.date.previousDay(date);
-		if (date < startDate) {
-			ret = {
-				'enabled': false,
-				'classes': '',
-				'tooltip': App.i18n.translate(
-					'End date must be greater than start date'
-				)
-			};
-		}
-	}
-	if (!ret) {
-		var quantity = Session.get('booking.form.booking.quantity') || 1;
-		var room = Session.get('booking.room');
-		ret = App.booking.datepicker.getAvailabilityData(room, date, quantity);
-	}
-	if (inRange) {
-		ret.classes += ' in-range';
-	}
-	return ret;
-};
-
 
 Template.bookingForm.rendered = function() {
-	$('.datepicker', this.firstNode).datepicker({
-		format: App.date.format.toLowerCase(),
-		todayHighlight: true,
-		todayBtn: false,
-		autoclose: true,
-		weekStart: 1,
-		startDate: App.date.today,
-		endDate: undefined,
-		keyboardNavigation: true,
-		forceParse: false,
-		beforeShowDay: beforeShowDay
-	});
+	App.booking.datepicker.initDatePickers(this.firstNode);
+	$('.form-control', this.firstNode).trigger('change');
 };
 
 Template.bookingForm.helpers({
