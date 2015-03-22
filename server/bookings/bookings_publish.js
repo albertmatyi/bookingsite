411dde96c7dd541dafa32d0697befa18f625192d
Matyas Albert Nagy <albertmatyi@gmail.com>
2015-03-22 17:22:29 +0100

    publish correct bookings

--- server/bookings/bookings_publish.js
+++ server/bookings/bookings_publish.js
@@ -1,6 +1,21 @@
 'use strict';
 
+var getBookingsInRange = function(startDate, endDate) {
+	// TODO return bookings in range
+	if (!startDate) {
+		startDate = moment().format(App.date.format);
+	}
+	var qry = {end: {$gt: startDate}};
+	if (endDate) {
+		qry.start = {$lt: endDate};
+	}
+	var cursor = App.bookings.collection.find(qry);
+	console.log(qry, cursor.count());
+	return cursor;
+};
+
 App.auth.meteor.publish('future-bookings', function() {
-	// TODO return future bookings
-	return App.bookings.collection.find();
+	return getBookingsInRange(+new Date(), null);
 });
+
+App.auth.meteor.publish('bookings', getBookingsInRange);
