5f03b60a143ac146a2b7f36ae05875067a79f911
Matyas Albert Nagy <albertmatyi@gmail.com>
2015-03-22 17:24:39 +0100

    fix moment blaze helper

--- client/lib/moment.js
+++ client/lib/moment.js
@@ -1,5 +1,8 @@
 'use strict';
 
 Blaze.registerHelper('moment', function(date, format) {
-	return moment(date).format(format || App.date.format);
+	if (!format || format.hash) {
+		format = App.date.format;
+	}
+	return moment(date).format(format);
 })
