7ecf19f7b8c63c48a097f64b9b50376b20b5e49d
Matyas Albert Nagy <albertmatyi@gmail.com>
2015-04-04 11:38:33 +0200

    add ro/hu translations for the booking form

--- client/views/booking/booking_i18n.js
+++ client/views/booking/booking_i18n.js
@@ -28,3 +28,57 @@ App.component('i18n.translations.en.booking').expose({
 		'Cancel': 'Cancel'
 	}
 });
+App.component('i18n.translations.hu.booking').expose({
+	title: 'Book',
+	form: {
+		'Full Name': 'Teljes név',
+		'Specify a valid full name': 'Adjon meg egy helyes nevet',
+		'Email': 'Email',
+		'Specify a valid email address': 'Adjon meg egy helyes email címet',
+		'Phone': 'Telefon',
+		'Specify a valid phone number': 'Adjon meg egy helyes telefonszámot',
+		'Citizenship': 'Nemzetiség',
+		'Specify your citizenship': 'Határozza meg nemzetiségét',
+		'Custom message': 'Üzenet',
+		'Quantity': 'Mennyiség',
+		'Number of guests': 'Vendégek száma',
+		'Arrival date': 'Érkezési dátum',
+		'Specify a valid date (dd-mm-yyyy)': 'Adjon meg egy helyes dátumot (dd-mm-yyyy)',
+		'The selected range contains overbooked days. Consider a different selection. ': 'A kiválasztott intervallum már túl van foglalva. Kérem válasszon egy másik dátumot vagy szobát',
+		'Departure date': 'Távozási dátum',
+		'Price': 'Ár',
+		'Per night': 'Per északa',
+		'Total': 'Total',
+		'Nights': 'Északák',
+		'Invalid data! See fields above.': 'Helytelen űrlap. Kérem ellenőrizze',
+		'Book': 'Foglalás',
+		'Cancel': 'Mégse'
+	}
+});
+App.component('i18n.translations.ro.booking').expose({
+	title: 'Book',
+	form: {
+		'Full Name': 'Nume complet',
+		'Specify a valid full name': 'Specificați numele complet',
+		'Email': 'Email',
+		'Specify a valid email address': 'Specificați o adresă validă de email',
+		'Phone': 'Phone',
+		'Specify a valid phone number': 'Specificați un număr valid de telefon',
+		'Citizenship': 'Cetățenia',
+		'Specify your citizenship': 'Specificați cetățenia',
+		'Custom message': 'Mesaj adițional',
+		'Quantity': 'Cantitate',
+		'Number of guests': 'Numărul oaspeților',
+		'Arrival date': 'Data sosirii',
+		'Specify a valid date (dd-mm-yyyy)': 'Specificați o dată validă (dd-mm-yyyy)',
+		'The selected range contains overbooked days. Consider a different selection. ': 'Intervalul selectat conține zile suprasolicitate. Vă rugăm să selectați o altă dată sau cameră',
+		'Departure date': 'Data plecării',
+		'Price': 'Preț',
+		'Per night': 'per noapte',
+		'Total': 'Total',
+		'Nights': 'Nopți',
+		'Invalid data! See fields above.': 'Date invalide. Verificați câmpurile de mai sus',
+		'Book': 'Rezervă',
+		'Cancel': 'Anulare'
+	}
+});
