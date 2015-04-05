106175b64640d96e1a8ca2d7816c88b4068e9d6c
Matyas Albert Nagy <albertmatyi@gmail.com>
2015-04-05 21:05:19 +0200

    add velocity & feedback form

--- client/views/feedbacks/feedbacks_i18n.js
+++ client/views/feedbacks/feedbacks_i18n.js
@@ -2,14 +2,45 @@
 
 App.component('i18n.translations.en.feedbacks').expose({
 	'description': 'If you would like to make a post please send us the text in e-mail and we\'ll take care of the rest.',//jshint ignore: line
-	'title': 'Guestbook'
+	'title': 'Guestbook',
+	'name': 'Name',
+	'Your full name': 'Your full name',
+	'email': 'Email',
+	'Your email address': 'Your email address',
+	'message': 'Message',
+	'The body of the message': 'The body of the message',
+	sendMessageTitle: 'Send a message',
+	sendMessage: 'Send',
+	messageSuccess: 'Message successfully sent',
+	leaveFeedback: 'Leave a feedback'
 });
 
 App.component('i18n.translations.ro.feedbacks').expose({
 	'description': 'Dacă doriți să lăsați un coment timiteți-ne un e-mail și noi ne ocupăm de rest.',//jshint ignore: line
-	'title': 'Impresii'
+	'title': 'Impresii',
+	name: 'Nume',
+	'Your full name': 'Your full name',
+	'email': 'Email',
+	'Your email address': 'Your email address',
+	'message': 'Message',
+	'The body of the message': 'The body of the message',
+	sendMessageTitle: 'Send a message',
+	sendMessage: 'Send',
+	messageSuccess: 'Message successfully sent',
+	leaveFeedback: 'Lasă o impresie'
 });
 App.component('i18n.translations.hu.feedbacks').expose({
 	'description': 'Ha bejegyzést szeretne írni a vendégkönybe kérem írja meg nekünk a szöveget e-mailben és mi megoldjuk a többit.',//jshint ignore: line
-	'title': 'Vendégkönyv'
+	'title': 'Vendégkönyv',
+	name: 'Név',
+	'Your full name': 'Your full name',
+	'email': 'Email',
+	'Your email address': 'Your email address',
+	'message': 'Message',
+	'The body of the message': 'The body of the message',
+	sendMessageTitle: 'Send a message',
+	sendMessage: 'Send',
+	messageSuccess: 'Message successfully sent',
+	leaveFeedback: 'Hagyjon egy üzenetet'
 });
+
